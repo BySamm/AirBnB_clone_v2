8a13e41f6f63eecadb4cdcb838b6918f32ee0367
BySamm <sam.mba.byiringiro@gmail.com>
2024-01-22 11:43:59 +0200

    Some changes added

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,8 +9,33 @@ if [ -d "$nginx_dir" ]; then
         echo "Nginx EXIST"
 else
 	echo "Nginx doesn't exist. Installing ..."
-	sudo apt update -y
-	sudo apt install -y ngnix
+
+        #  updates the package lists for available software packages.
+        sudo apt-get update -y
+        # installs the Nginx web server.
+        sudo apt-get install -y nginx
+        # starting nginx service
+        sudo service nginx start
+        # allowing nginx on firewall
+        sudo ufw allow 'Nginx HTTP'
+        # Give the user ownership to website files for easy editing
+        sudo chown -R "$USER":"$USER" /var/www/html
+        sudo chmod -R 755 /var/www
+
+        # Backup default index
+        cp /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.bckp
+
+        #  sets the content of the index.html file to "Hello World!" using the echo command.
+        echo "Hello World!" > /var/www/html/index.nginx-debian.html
+
+        # Sets Nginx to listen on port 80 and serve files from the default directory.
+        printf %s "server {
+                listen      80;
+                listen      [::]:80 default_server;
+                root        /var/www/html;
+                index       index.nginx-debian.html index.html index.htm;
+        }
+        " > /etc/nginx/sites-enabled/default
 fi
 
 mkdir -p /data
